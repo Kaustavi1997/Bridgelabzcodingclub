1cde7baff5ea5a2d1a510b0de9edb995a8c85fa2
“Kaustavi <kaustavisen8@gmail.com>
2020-05-17 17:32:15 +0530

    switchcase

---
+++ switchcase.sh
@@ -0,0 +1,37 @@
+#! /bin/bash -x
+present=1
+absent=0
+isparttime=1
+isfulltime=0
+empwageperhour=20
+workingdays=20
+a=$((RANDOM%2))
+if [ $a -eq $present ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
+
+if [ $a -eq 0 ]
+then
+	empcheck=2
+else
+	empcheck=$((RANDOM%2))
+fi
+
+case $empcheck in 
+	$isfulltime)
+		emphrs=8	
+		;;
+	$isparttime)
+		emphrs=4
+		;;
+	*)
+		emphrs=0
+		;;
+
+esac
+salaryperday=$(($emphrs*$empwageperhour))
+echo $salaryperday
+
